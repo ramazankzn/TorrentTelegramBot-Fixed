be57af6823bc91ef183df8f89c620753e0475fb9
Ram <86193880+ramazankzn@users.noreply.github.com>
2025-10-28 14:52:39 +0300

    Add TelegramClient configuration

---
+++ src/main/java/com/torrentbot/config/BotConfig.java
@@ -0,0 +1,19 @@
+package com.torrentbot.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+@Configuration
+public class BotConfig {
+    
+    @Value("${bot.token}")
+    private String botToken;
+    
+    @Bean
+    public TelegramClient telegramClient() {
+        return new OkHttpTelegramClient(botToken);
+    }
+}
